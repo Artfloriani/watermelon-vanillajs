86a3f7e5ebe928e7999b598caf2f1bac185bb882
Arthur Floriani <artfloriani@gmail.com>
2023-10-19 21:44:10 -0500

    feat: use physics engine

--- src/fruit.ts
+++ src/fruit.ts
@@ -1,15 +1,16 @@
+import * as p2 from "p2-es";
+
 export class Fruit {
   constructor(
     public x: number,
     public y: number,
     public radius: number = 10,
     public color: string = "green",
-    public type = 0
+    public type = 0,
+    public body?: p2.Body,
+    public shape?: p2.Circle
   ) {
-    this.dy = 0; // initial vertical speed
-    this.dx = 0; // initial horizontal speed
-
-    const chance = Math.round(Math.random() * 15);
+    const chance = Math.round(Math.random() * 1);
 
     if (chance < 5) {
       this.type = 0;
@@ -23,31 +24,16 @@ export class Fruit {
 
     this.updateType();
   }
-  dy: number;
-  dx: number;
-  acceleration: number = 0.1; // gravitational acceleration
-  friction: number = 0.05; // friction coefficient
-
-  hasCollided: boolean = false;
-  collisionCooldown: number = 0; // in frames
-
-  collidesWith(other: Fruit): boolean {
-    const dx = this.x - other.x;
-    const dy = this.y - other.y;
-    const distance = Math.sqrt(dx * dx + dy * dy);
-    return distance <= this.radius + other.radius;
-  }
 
   updateType() {
-    console.log("yes");
     switch (this.type) {
       case 0:
-        this.color = "#228B22"; // Unripe, Green
-        this.radius = 10;
+        this.color = "#00FF00"; // Unripe, Green
+        this.radius = 1;
         break;
       case 1:
         this.color = "#FFFF00"; // Ripe, Yellow
-        this.radius = 15;
+        this.radius = 1.5;
         break;
       case 2:
         this.color = "#FFA500"; // Juicy, Orange
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import "./style.css";
 import { setupCounter } from "./counter.ts";
 import { Fruit } from "./fruit.ts";
-import { resolveCollision } from "./physics.ts";
+
+import * as p2 from "p2-es";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     <canvas id="gameCanvas" width="400" style="border: 1px solid red;" height="400"></canvas>
@@ -12,9 +13,15 @@ const canvas: HTMLCanvasElement = <HTMLCanvasElement>(
   document.getElementById("gameCanvas")
 );
 const ctx = canvas?.getContext("2d");
+let w: number, h: number;
+var scaleX = 50,
+  scaleY = -50;
+
+let world: p2.World;
 
 // Fruits array
 let fruits: Fruit[] = [];
+let markedForDeletion: Fruit[] = [];
 
 // Click event to drop fruit
 canvas.addEventListener("click", function (event) {
@@ -23,91 +30,140 @@ canvas.addEventListener("click", function (event) {
   dropFruit(x, y);
 });
 
-const gravity = 0.1;
-const friction = 0.05;
+// Convert a canvas coordiante to physics coordinate
+function getPhysicsCoord(px: number, py: number) {
+  var rect = canvas.getBoundingClientRect();
+  var x = px - rect.left;
+  var y = py - rect.top;
+
+  x = (x - w / 2) / scaleX;
+  y = (y - h / 2) / scaleY;
+
+  return [x, y];
+}
 
 // Function to drop a fruit
 function dropFruit(x: number, y: number) {
   const newFruit = new Fruit(x, y);
+
+  const circleShape = new p2.Circle({ radius: newFruit.radius });
+  const position = getPhysicsCoord(x + newFruit.radius * scaleX, y);
+  const circleBody = new p2.Body({ mass: 1, position });
+  circleBody.addShape(circleShape);
+  world.addBody(circleBody);
+
+  newFruit.body = circleBody;
+  newFruit.shape = circleShape;
   fruits.push(newFruit);
 }
-// Animation loop
-function animate() {
-  requestAnimationFrame(animate);
 
-  clearCanvas();
+let planeShape: p2.Shape, planeBody: p2.Body | null;
+
+function init() {
+  if (canvas === null || ctx === null) return;
+  // Init canvas
+  w = canvas.width;
+  h = canvas.height;
+
+  ctx.lineWidth = 0.05;
+
+  // Init p2.js
+  world = new p2.World();
+
+  // Add a plane
+  planeShape = new p2.Plane();
+  planeBody = new p2.Body({ position: [0, -4] });
+  planeBody.addShape(planeShape);
+  world.addBody(planeBody);
+
+  world.on("beginContact", function (event) {
+    const shapeA: p2.Circle = event.shapeA,
+      shapeB: p2.Circle = event.shapeB;
+    if (shapeA.radius !== undefined && shapeB.radius !== undefined) {
+      checkMerge(shapeA, shapeB);
+    }
+  });
+}
+
+function render() {
+  if (ctx == null) return;
+  // Clear the canvas
+  ctx.clearRect(0, 0, w, h);
+
+  // Transform the canvas
+  // Note that we need to flip the y axis since Canvas pixel coordinates
+  // goes from top to bottom, while physics does the opposite.
+  ctx.save();
+  ctx.translate(w / 2, h / 2); // Translate to the center
+  ctx.scale(scaleX, scaleY); // Zoom in and flip y axis
 
   for (let fruit of fruits) {
-    // Simple gravity
-    worldPhysics(fruit);
-
-    checkCollisions(fruit);
-
-    // draw
     drawFruit(fruit);
   }
+
+  // Draw all bodies
+  drawPlane();
+
+  // Restore transform
+  ctx.restore();
 }
 
-function checkCollisions(f: Fruit) {
-  for (let i = 0; i < fruits.length; i++) {
-    const other = fruits[i];
-    if (f !== other && f.collidesWith(other)) {
-      if (f.color === other.color) {
-        combineFruits(f, other);
-      }
-      resolveCollision(f, other);
+var lastTime = 0,
+  timeStep = 1 / 60,
+  maxSubSteps = 5;
+
+// Animation loop
+function animate(time: number) {
+  requestAnimationFrame(animate);
+
+  var dt = lastTime ? (time - lastTime) / 1000 : 0;
+  dt = Math.min(1 / 10, dt);
+  lastTime = time;
+
+  // Move physics bodies forward in time
+  world.step(timeStep, dt, maxSubSteps);
+
+  for (let fruit of markedForDeletion) {
+    if (fruit.body) {
+      world.removeBody(fruit.body);
+      fruits = fruits.filter((f) => f !== fruit);
     }
   }
-}
 
-function combineFruits(f1: Fruit, f2: Fruit) {
-  f1.x = (f1.x + f2.x) / 2;
-  f1.y = (f1.y + f2.y) / 2;
-  f1.dy = (f1.dy + f2.dy) / 2;
-  f1.dx = (f1.dx + f2.dx) / 2;
-  f1.type++;
-  f1.updateType();
-  fruits = fruits.filter((f) => f !== f2);
-}
-
-function clearCanvas() {
-  if (ctx == null) return;
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // Render scene
+  render();
 }
 
 function drawFruit(f: Fruit) {
-  if (ctx == null) return;
+  if (ctx == null || !f.body || !f.shape) return;
   ctx.beginPath();
-  ctx.arc(f.x, f.y, f.radius, 0, Math.PI * 2);
+  const x = f.body.interpolatedPosition[0],
+    y = f.body.interpolatedPosition[1],
+    radius = f.shape.radius;
+  ctx.arc(x, y, radius, 0, Math.PI * 2);
   ctx.fillStyle = f.color;
   ctx.fill();
   ctx.closePath();
 }
 
-function worldPhysics(f: Fruit) {
-  f.dy += gravity;
-  f.y += f.dy;
-  f.x += f.dx;
-  if (f.y + f.radius > canvas.height) {
-    f.y = canvas.height - f.radius;
-    f.dy = -0.3 * f.dy; // Bounce back up, lose some speed
+function drawPlane() {
+  if (planeBody == null) return;
+  if (ctx == null) return;
+  var y = planeBody.interpolatedPosition[1];
+  ctx.moveTo(-w, y);
+  ctx.lineTo(w, y);
+  ctx.stroke();
+}
 
-    // apply some friction at the ground
-    if (Math.abs(f.dx) > 0) {
-      if (f.dx > 0) f.dx -= friction;
-      else f.dx += friction;
-
-      if (Math.abs(f.dx) < friction) f.dx = 0;
+function checkMerge(shapeA: p2.Circle, shapeB: p2.Circle) {
+  if (shapeA.radius == shapeB.radius) {
+    const fruitA = fruits.find((f) => f.shape === shapeA);
+    const fruitB = fruits.find((f) => f.shape === shapeB);
+    if (fruitA && fruitB) {
+      markedForDeletion.push(fruitA);
     }
-  }
-
-  if (f.x + f.radius > canvas.width) {
-    f.x = canvas.width - f.radius;
-    f.dx = -0.3 * f.dx;
-  } else if (f.x - f.radius < 0) {
-    f.x = f.radius;
-    f.dx = -0.3 * f.dx;
   }
 }
 
-animate();
+init();
+requestAnimationFrame(animate);
